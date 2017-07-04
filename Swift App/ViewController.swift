5607811782c6b67635b9ac3103aa6156b268e1eb
Sean Moorman <seanmoorman@seans-mbp.attlocal.net>
2017-07-04 17:47:44 -0500

    my first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+    @IBAction func theButton(_ sender: Any) {
+        
+        theLabel.text = "Hello There!"
+        print("Button tapped")
+    }
+    
+    @IBAction func theButton2(_ sender: Any) {
+        
+        theLabel.text = " Buttons are Cool!"
+        print("Button tapped")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+        
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+    
+
+
 
